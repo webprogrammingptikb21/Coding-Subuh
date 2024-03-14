8c18b6d47e057191e5148299711a9f7d825b9f51
webprogrammingptikb21 <alwi3479@gmail.com>
2024-03-14 23:31:15 +0800

    menambahkan latihan

---
+++ pertemuan 2/output.js
@@ -0,0 +1,18 @@
+console.log("program dimulai...");
+console.info("tampilkan teks di ke elemen H1");
+headLine.innerText = "latihan fungsi output";
+console.info("done, sekarang teks sudah ditampilkan di h1");
+
+// menampilkan output ke document
+document.write("<p>Belajar javaScript itu gampang!</p>");
+// document.write("<p>silahkan cek console Javacript untuk output lainya</p>");
+// document.write("<p>tekan <kbd></kbd></p>");
+
+// mencoba console info, war, error, debug
+console.info("website ini sudah aman");
+console.error("HACKER JANGAN MENYERANG");
+console.warn("menyerang website tanpa izin adalah ilegal");
+console.debug("Isi object headline:", headLine);
+
+// menampilkan output dengan alert
+alert("Welcome");
