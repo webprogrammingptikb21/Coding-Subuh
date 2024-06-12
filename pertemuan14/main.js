00ee8f986e07640b143ff559658c92d74c3784ea
webprogrammingptikb21 <alwi3479@gmail.com>
2024-06-13 01:24:01 +0800

    lanjutan javascript

--- pertemuan14/main.js
+++ pertemuan14/main.js
@@ -1,9 +1,16 @@
 // cara buat object product dan cart
 const keranjang = new Cart();
 
+// buat object wallet dengan saldo awal 10 rb
+const dana = new Wallet(10000);
+
 const iphone = new Product("iphone 15 pro max", "33000000", "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-15-pro-max.jpg");
 
 const vivo = new Product("Vivo v23 E", "4000000", "https://fdn2.gsmarena.com/vv/bigpic/vivo-v23e.jpg");
 
 // tambahkan iphone ke keranjang
 keranjang.addItem(iphone);
+
+// topup
+dana.topUp(5000000);
+console.log(dana.getCurrentBalanceIDR());
--- pertemuan15/Wallet.js
+++ pertemuan15/Wallet.js
@@ -5,42 +5,46 @@ class Wallet {
 
   topUp(amount) {
     if (amount < 0 && isNaN(amount)) {
-      console.error("Jumlah TopUp salah");
+      console.error("Jumlah topup salah!");
       return;
     }
 
     this.balance += amount;
   }
-  widthDraw(amount) {
+
+  withdraw(amount) {
     if (isNaN(amount) && amount > this.balance && amount < 0) {
-      console.error("Jumlah Penarikan Salah");
+      console.error("Jumlah penarikan salah!");
       return;
     }
+
     this.balance -= amount;
   }
+
   pay(amount) {
-    const pin = Number(prompt("inputkan PIN"));
-    // amount harus angka dan lebih besar dari 0 dan lebih kecil dari saldo
+    const pin = Number(prompt("Inputkan PIN"));
+    // amount: harus angka dan lebih besar dari 0 dan lebih kecil dari saldo
     const validAmount = !isNaN(amount) && amount < this.balance && amount > 0;
-    const isPiCorrect = pin === 123456;
+    const isPinCorrect = pin === 123456;
 
-    if (!isPiCorrect) {
-      alert("Pin Salah, Silahkan masukkan pin yang benar");
+    if (!isPinCorrect) {
+      alert("PIN Salah");
       return;
     }
 
-    if (validAmount) {
-      alert("Saldo tidak cukup, Silahkan top up");
+    if (!validAmount) {
+      alert("Saldo tidak cukup, silahkan topup dulu!");
       return;
     }
 
-    if (validAmount && isPiCorrect) {
-      this.balance -= amount;
-    }
+    this.balance -= amount;
+    document.dispatchEvent(new Event("payment-success"));
   }
+
   getCurrentBalance() {
     return this.balance;
   }
+
   getCurrentBalanceIDR() {
     return this.balance.toLocaleString("id", {
       style: "currency",
